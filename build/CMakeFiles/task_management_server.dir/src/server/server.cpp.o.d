688f006af116062f575e3264bac11a72beec420b
Segren <geninse96@gmail.com>
2024-06-06 19:38:08 +0300

    final commit

--- build/CMakeFiles/task_management_server.dir/src/server/server.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/task_management_server.dir/src/server/server.cpp.o: \
-  /Users/dgon/Documents/task_management_server/src/server/server.cpp
--- build/CMakeFiles/task_management_server.dir/src/utils/auth.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/task_management_server.dir/src/utils/auth.cpp.o: \
-  /Users/dgon/Documents/task_management_server/src/utils/auth.cpp
